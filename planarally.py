c5f0913cfb520de2852491c98b5dd125f24bc3f7
darragh <kruptein@gmail.com>
2018-02-21 19:47:27 +0100

    WOOO sync stuff works

--- planarally.py
+++ planarally.py
@@ -4,6 +4,7 @@ class Client:
         self.rooms = [sid]
         self.initialised = False
 
+
 class LayerManager:
     def __init__(self):
         self.layers = []
@@ -16,18 +17,20 @@ class LayerManager:
             'layers': [l.as_dict() for l in self.layers]
         }
 
+
 class Layer:
     def __init__(self, name):
         self.shapes = []
 
     def add_shape(self, shape):
-        self.shapes.append(shape)
+        self.shapes.append(shape.as_dict())
 
     def as_dict(self):
         return {
-            'shapes': [s.as_dict() for s in self.shapes],
+            'shapes': self.shapes,
             'grid': False
         }
+
 
 class GridLayer(Layer):
     def __init__(self, name):
@@ -35,6 +38,7 @@ class GridLayer(Layer):
 
     def as_dict(self):
         return {'grid': True}
+
 
 class Shape:
     def __init__(self, x, y, width, height, colour="green"):
@@ -53,6 +57,7 @@ class Shape:
             'c': self.colour
         }
 
+
 class PlanarAlly:
     def __init__(self):
         self.clients = {}
@@ -62,10 +67,10 @@ class PlanarAlly:
         self.layer_manager.add(Layer("tokens"))
         self.layer_manager.add(Layer("dm"))
         self.layer_manager.add(GridLayer("grid"))
-        self.layer_manager.layers[0].add_shape(Shape(40,40,50,50))
-        self.layer_manager.layers[1].add_shape(Shape(80,40,50,50, "red"))
-        self.layer_manager.layers[1].add_shape(Shape(40,80,50,50, "red"))
-        self.layer_manager.layers[2].add_shape(Shape(80,80,50,50, "blue"))
+        self.layer_manager.layers[0].add_shape(Shape(40, 40, 50, 50))
+        self.layer_manager.layers[1].add_shape(Shape(80, 40, 50, 50, "red"))
+        self.layer_manager.layers[1].add_shape(Shape(40, 80, 50, 50, "red"))
+        self.layer_manager.layers[2].add_shape(Shape(80, 80, 50, 50, "blue"))
 
     def add_client(self, sid):
         self.clients[sid] = Client(sid)
--- planarserver.py
+++ planarserver.py
@@ -12,6 +12,8 @@ sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
 
+print(PA.layer_manager.as_dict())
+
 
 async def index(request):
     with open('planarally.html') as f:
@@ -22,54 +24,28 @@ async def index(request):
 async def client_init(sid):
     PA.clients[sid].initialised = True
 
+
 @sio.on('layer invalidate', namespace='/planarally')
 async def layer_invalid(sid, message):
     if PA.clients[sid].initialised:
         PA.layer_manager.layers[message['layer']].shapes = message['shapes']
-        await sio.emit('layer set', PA.layer_manager.as_dict(), room=sid, namespace='/planarally')
-
-
-@sio.on('my broadcast event', namespace='/planarally')
-async def test_broadcast_message(sid, message):
-    await sio.emit('my response', {'data': message['data']}, namespace='/planarally')
-
-
-@sio.on('join', namespace='/planarally')
-async def join(sid, message):
-    sio.enter_room(sid, message['room'], namespace='/planarally')
-    await sio.emit('my response', {'data': 'Entered room: ' + message['room']},
-                   room=sid, namespace='/planarally')
-
-
-@sio.on('leave', namespace='/planarally')
-async def leave(sid, message):
-    sio.leave_room(sid, message['room'], namespace='/planarally')
-    await sio.emit('my response', {'data': 'Left room: ' + message['room']},
-                   room=sid, namespace='/planarally')
-
-
-@sio.on('close room', namespace='/planarally')
-async def close(sid, message):
-    await sio.emit('my response',
-                   {'data': 'Room ' + message['room'] + ' is closing.'},
-                   room=message['room'], namespace='/planarally')
-    await sio.close_room(message['room'], namespace='/planarally')
-
-
-@sio.on('my room event', namespace='/planarally')
-async def send_room_message(sid, message):
-    await sio.emit('my response', {'data': message['data']},
-                   room=message['room'], namespace='/planarally')
+        print(message['shapes'])
+        print(PA.layer_manager.as_dict())
+        d = PA.layer_manager.layers[message['layer']].as_dict()
+        d['layer'] = message['layer']
+        await sio.emit('layer set', d, room="skt", skip_sid=sid, namespace='/planarally')
 
 
 @sio.on('disconnect request', namespace='/planarally')
 async def disconnect_request(sid):
+    sio.leave_room(sid, "skt", namespace='/planarally')
     await sio.disconnect(sid, namespace='/planarally')
 
 
 @sio.on('connect', namespace='/planarally')
 async def test_connect(sid, environ):
     PA.add_client(sid)
+    sio.enter_room(sid, "skt", namespace='/planarally')
     await sio.emit('board init', PA.layer_manager.as_dict(), room=sid, namespace='/planarally')
 
 
@@ -81,10 +57,10 @@ def test_disconnect(sid):
 app.router.add_static('/static', 'static')
 app.router.add_get('/', index)
 
-
 if __name__ == '__main__':
     if os.path.isdir("cert"):
         import ssl
+
         ctx = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS)
         ctx.load_cert_chain("cert/fullchain.pem", "cert/privkey.pem")
         web.run_app(app, port=8000, ssl_context=ctx)

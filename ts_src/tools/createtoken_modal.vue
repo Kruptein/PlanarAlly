25cc7bb0b922ef1c420103440e2abb011011d51c
darragh <kruptein@gmail.com>
2018-08-10 09:21:16 +0200

    Work towards a create token vue modal

---
+++ ts_src/tools/createtoken_modal.vue
@@ -0,0 +1,68 @@
+<template>
+    <modal :visible="visible" @close="close">
+        <div class='modal-header'>
+            Create basic token
+        </div>
+        <div class='modal-body'>
+            <label for="createtokendialog-text">Text</label>
+            <input type="text" id="createtokendialog-name">
+            <label for="createtokendialog-fill">Fill</label>
+            <input type="text" id="createtokendialog-fill" title="Fill colour">
+            <label for="createtokendialog-border">Border</label>
+            <input type="text" id="createtokendialog-border" title="Border colour">
+        </div>
+        <div class='modal-footer'>
+            <button @click="submit">Submit</button>
+        </div>
+    </modal>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import modal from "../../vue/components/modals/modal.vue";
+
+export default Vue.component('createtoken-modal', {
+    components: {
+        modal
+    },
+    data: function () {
+        return {
+            visible: false,
+            resolve: () => {},
+            reject: () => {}
+        }
+    },
+    methods: {
+        submit: function () {
+            this.resolve();
+            this.close();
+        },
+        close: function () {
+            this.reject();
+            this.visible = false;
+        },
+    }
+})
+</script>
+
+<style scoped>
+.modal-header {
+    background-color: #FF7052;
+    padding: 10px;
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.modal-body {
+    padding: 10px;
+    padding-bottom: 0;
+    display: flex;
+    justify-content: space-between;
+}
+
+.modal-footer {
+    padding-top: 0;
+    padding: 10px;
+    text-align: right;
+}
+</style>
--- ts_src/tools/tools.ts
+++ ts_src/tools/tools.ts
@@ -97,49 +97,4 @@ const app = new Vue({
 
 export function setupTools(): void {
     app.toolsLoaded = true;
-    // console.log("setup");
-    // app.$emit("loadTools");
-    // const toolselectDiv = $("#toolselect").find("ul");
-    // tools.forEach(function (tool) {
-    //     if (!tool.playerTool && !Settings.IS_DM) return;
-
-    //     const toolInstance = new tool.clz();
-    //     gameManager.tools.set(tool.name, toolInstance);
-    //     const extra = tool.defaultSelect ? " class='tool-selected'" : "";
-    //     const toolLi = $("<li id='tool-" + tool.name + "'" + extra + "><a href='#'>" + capitalize(tool.name) + "</a></li>");
-    //     toolselectDiv.append(toolLi);
-    //     if (tool.hasDetail && toolInstance.detailDiv !== undefined) {
-    //         const div = toolInstance.detailDiv;
-    //         $('#tooldetail').append(div);
-    //         div.hide();
-    //     }
-    //     toolLi.on("click", function () {
-    //         const index = tools.indexOf(tool);
-    //         if (index !== gameManager.selectedTool) {
-    //             gameManager.tools.getIndexValue(gameManager.selectedTool).onDeselect();
-    //             $('.tool-selected').removeClass("tool-selected");
-    //             $(this).addClass("tool-selected");
-    //             gameManager.selectedTool = index;
-    //             const detail = $('#tooldetail');
-    //             if (tool.hasDetail) {
-    //                 $('#tooldetail').children().hide();
-    //                 toolInstance.detailDiv!.show();
-    //                 detail.show();
-    //             } else {
-    //                 detail.hide();
-    //             }
-    //             gameManager.tools.getIndexValue(gameManager.selectedTool).onSelect();
-    //         }
-    //     });
-    // });
 }
-
-// const tools = [
-//     { name: "select", playerTool: true, defaultSelect: true, hasDetail: false, clz: SelectTool },
-//     { name: "pan", playerTool: true, defaultSelect: false, hasDetail: false, clz: PanTool },
-//     { name: "draw", playerTool: true, defaultSelect: false, hasDetail: true, clz: DrawTool },
-//     { name: "ruler", playerTool: true, defaultSelect: false, hasDetail: false, clz: RulerTool },
-//     { name: "fow", playerTool: false, defaultSelect: false, hasDetail: true, clz: FOWTool },
-//     { name: "brush", playerTool: false, defaultSelect: false, hasDetail: true, clz: BrushTool },
-//     { name: "map", playerTool: false, defaultSelect: false, hasDetail: true, clz: MapTool },
-// ];

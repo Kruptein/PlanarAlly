82bb0631f0f32816b07922a153b92ad6dcc6ec55
darragh <kruptein@gmail.com>
2018-08-23 01:13:35 +0200

    Remove .js.map and .js due to wrong build location

--- ts_src/game/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-$(".input").focusin(function () {
-    $(this).find("span").animate({ "opacity": "0" }, 200);
-});
-$(".input").focusout(function () {
-    $(this).find("span").animate({ "opacity": "1" }, 300);
-});
-//# sourceMappingURL=login.js.map

099f6a48f5ca335829daa3c61f3e551defa59d03
darragh <kruptein@gmail.com>
2018-09-07 14:52:39 +0200

    Only show map tool to DM

--- ts_src/game/ui/tools/tools.vue
+++ ts_src/game/ui/tools/tools.vue
@@ -4,6 +4,7 @@
             <ul>
                 <li
                     v-for="tool in tools"
+                    v-if="!dmTools.includes(tool) || IS_DM"
                     :key="tool"
                     :class="{'tool-selected': currentTool === tool}"
                     :ref="tool + '-selector'"
@@ -54,6 +55,7 @@ export default Vue.component("tools", {
     data: () => ({
         currentTool: "select",
         tools: ["select", "pan", "draw", "ruler", "map"],
+        dmTools: ["map"],
     }),
     watch: {
         currentTool(newValue, oldValue) {

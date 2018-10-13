0210c298ef60de1d1834dc571f894e101cc7588b
Darragh Van Tichelen <darragh.van.tichelen@theoplayer.com>
2018-10-13 15:18:29 +0200

    Merge UserOption and User

--- PlanarAlly/models/__init__.py
+++ PlanarAlly/models/__init__.py
@@ -3,12 +3,12 @@ from config import SAVE_FILE
 
 db = SqliteDatabase(SAVE_FILE)
 
-from .user import User, UserOption
+from .user import User
 from .campaign import Room, Location, LocationUserOption, Layer, Shape, PlayerRoom, Tracker, Aura, ShapeOwner
 from .general import Constants
 from .asset import Asset
 
-ALL_MODELS = [User, UserOption, Room, Location, LocationUserOption, Layer, Shape,
+ALL_MODELS = [User, Room, Location, LocationUserOption, Layer, Shape,
               PlayerRoom, Tracker, Aura, ShapeOwner, Constants, Asset]
 
 

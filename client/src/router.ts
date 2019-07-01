b746c7109ece02dc98f3de29588f26884a668160
Darragh Van Tichelen <kruptein@gmail.com>
2019-07-01 13:12:46 +0200

    Fix import order

--- client/src/router.ts
+++ client/src/router.ts
@@ -5,13 +5,12 @@ import Vue from "vue";
 import Router from "vue-router";
 
 Vue.use(Router);
-
-import AccountSettings from "@/dashboard/settings.vue";
 import AssetManager from "@/assetManager/manager.vue";
 import Login from "@/auth/login.vue";
 import Logout from "@/auth/logout";
 import LoadComponent from "@/core/components/load.vue";
 import Dashboard from "@/dashboard/main.vue";
+import AccountSettings from "@/dashboard/settings.vue";
 import Game from "@/game/game.vue";
 import Invitation from "@/invitation/invitation";
 

ded54c8d60fc02c808177e490389821478646ecb
Darragh Van Tichelen <info@darragh.dev>
2019-09-21 00:06:56 +0200

    Change jest testMatch

--- client/jest.config.js
+++ client/jest.config.js
@@ -5,12 +5,12 @@ module.exports = {
         ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
         "^.+\\.ts$": "ts-jest",
     },
-    transformIgnorePatterns: ["/node_modules/(?!@babel)", "/dist/"],
+    transformIgnorePatterns: ["/node_modules/(?!@babel)"],
     moduleNameMapper: {
         "^@/(.*)$": "<rootDir>/src/$1",
     },
     snapshotSerializers: ["jest-serializer-vue"],
-    testMatch: ["**/tests/unit/**/*.test.(js|ts)|**/__tests__/*.(js|ts)"],
+    testMatch: ["<rootDir>/tests/unit/**/*.test.(js|ts)|**/__tests__/*.(js|ts)"],
     testURL: "http://localhost/",
     watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
     globals: {

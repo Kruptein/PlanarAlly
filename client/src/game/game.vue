977e0c035c97c384c9901098545d2db75bb3586b
Zach Myers <zachmyers3@gmail.com>
2020-02-19 22:02:12 -0500

    adjusted pinch zoom scaling factor for smoother feel

--- client/src/game/game.vue
+++ client/src/game/game.vue
@@ -178,7 +178,10 @@ export default class Game extends Vue {
     pinch(e: any): void {
         if (!e.target || !(<HTMLElement>e.target).tagName || (<HTMLElement>e.target).tagName !== "CANVAS") return;
         const delta = Math.sign(e.deltaY) * -1;
-        gameStore.updateZoom({ newZoomDisplay: gameStore.zoomDisplay - 0.1 * delta, zoomLocation: l2g(getLocalPointFromEvent(e.srcEvent)) });
+        gameStore.updateZoom({
+            newZoomDisplay: gameStore.zoomDisplay - 0.005 * delta,
+            zoomLocation: l2g(getLocalPointFromEvent(e.srcEvent)),
+        });
     }
 
     touchend(event: TouchEvent): void {

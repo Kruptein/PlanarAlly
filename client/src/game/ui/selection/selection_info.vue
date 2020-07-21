5bf03f73a2d9f25690ba27dbcb628c71a489f451
hmqgg <mitchx7@outlook.com>
2020-07-21 13:14:00 +0800

    Fix lint

--- client/src/game/ui/selection/selection_info.vue
+++ client/src/game/ui/selection/selection_info.vue
@@ -55,7 +55,7 @@ export default class SelectionInfo extends Vue {
         if (this.shape === null) return;
         const value = await (<Game>this.$parent.$parent).$refs.prompt.prompt(
             this.$t("game.ui.selection.select_info.new_value_NAME", { name: object.name }).toString(),
-            this.$t("game.ui.selection.select_info.updating_NAME", { name: object.name }).toString()
+            this.$t("game.ui.selection.select_info.updating_NAME", { name: object.name }).toString(),
         );
         if (this.shape === null) return;
         const ogValue = object.value;

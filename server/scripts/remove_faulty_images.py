48989c5566342081b697b352b55834cd70036f86
Darragh Van Tichelen <info@darragh.dev>
2020-08-16 17:04:01 +0200

    Restrict asset drop to /static images

---
+++ server/scripts/remove_faulty_images.py
@@ -0,0 +1,24 @@
+"""
+This is a simple script responsible for removing assets that got added as assets to your game by dragging something (e.g. some html element) on the board.
+These should not trigger shape creation, but did in the past and would spam warnings in the console when drawing the game board.
+
+This script should in principle only be ran once after upgrading to 0.22.0. (or regularly if you are on an earlier version and annoyed by the messages).
+
+Usage: 
+"""
+import sys
+from pathlib import Path
+
+# Insert parent folder in the lookup
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+from models import AssetRect
+
+
+def remove_assets():
+    removed = AssetRect.delete().where(AssetRect.src.startswith("/game/")).execute()
+    print(f"Removed {removed} assets that should not exist.")
+
+
+if __name__ == "__main__":
+    remove_assets()

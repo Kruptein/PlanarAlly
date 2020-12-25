a61738434d143308a35c54f2592cad535c4537f9
Darragh Van Tichelen <info@darragh.dev>
2020-12-25 15:38:52 +0100

    Restructure some socket logic

---
+++ client/src/core/socket.ts
@@ -0,0 +1,12 @@
+import { Manager } from "socket.io-client";
+import { BASE_PATH } from "../utils";
+
+export function createNewManager(): Manager {
+    return new Manager(location.protocol + "//" + location.host, {
+        autoConnect: false,
+        path: BASE_PATH + "socket.io",
+        transports: ["websocket", "polling"],
+    });
+}
+
+export const socketManager = createNewManager();

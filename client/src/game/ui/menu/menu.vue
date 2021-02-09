6debff0c53f6ac1a489b42fbe7ce9cc7d2f99b13
Darragh Van Tichelen <info@darragh.dev>
2021-02-09 12:38:10 +0100

    Move client options to a modal

--- client/src/game/ui/menu/menu.vue
+++ client/src/game/ui/menu/menu.vue
@@ -4,7 +4,6 @@ import Component from "vue-class-component";
 import { mapState } from "vuex";
 
 import ColorPicker from "@/core/components/colorpicker.vue";
-import LanguageSelect from "@/core/components/languageSelect.vue";
 import { baseAdjust, uuidv4 } from "@/core/utils";
 import { Note } from "@/game/comm/types/general";
 import { layerManager } from "@/game/layers/manager";
@@ -19,7 +18,6 @@ import UI from "../ui.vue";
     components: {
         "color-picker": ColorPicker,
         "asset-node": AssetNode,
-        LanguageSelect,
         NoteDialog,
     },
     computed: {
@@ -45,37 +43,7 @@ export default class MenuBar extends Vue {
     get IS_DM(): boolean {
         return gameStore.IS_DM || gameStore.FAKE_PLAYER;
     }
-    get gridColour(): string {
-        return gameStore.gridColour;
-    }
-    set gridColour(value: string) {
-        gameStore.setGridColour({ colour: value, sync: true });
-    }
-    get fowColour(): string {
-        return gameStore.fowColour;
-    }
-    set fowColour(value: string) {
-        gameStore.setFOWColour({ colour: value, sync: true });
-    }
-    get rulerColour(): string {
-        return gameStore.rulerColour;
-    }
-    set rulerColour(value: string) {
-        gameStore.setRulerColour({ colour: value, sync: true });
-    }
-    get invertAlt(): boolean {
-        return gameStore.invertAlt;
-    }
-    set invertAlt(value: boolean) {
-        gameStore.setInvertAlt({ invertAlt: value, sync: true });
-    }
-    get gridSize(): number {
-        return gameStore.gridSize;
-    }
-    set gridSize(gridSize: number) {
-        if (gridSize < 1) return;
-        gameStore.setGridSize({ gridSize, sync: true });
-    }
+
     settingsClick(event: { target: HTMLElement }): void {
         if (
             event.target.classList.contains("menu-accordion") &&
@@ -95,7 +63,10 @@ export default class MenuBar extends Vue {
 
     openDmSettings(): void {
         this.$parent.$refs.dmsettings.open();
-        EventBus.$emit("DmSettings.Open");
+    }
+
+    openUserSettings(): void {
+        this.$parent.$refs.clientsettings.open();
     }
 
     delMarker(marker: string): void {
@@ -174,23 +145,7 @@ export default class MenuBar extends Vue {
                 </div>
             </div>
             <!-- CLIENT OPTIONS -->
-            <button class="menu-accordion" v-t="'game.ui.menu.menu.client_options'"></button>
-            <div class="menu-accordion-panel">
-                <div class="menu-accordion-subpanel">
-                    <label for="gridSize" v-t="'game.ui.menu.menu.grid_size_in_pixels'"></label>
-                    <div><input id="gridSize" type="number" v-model="gridSize" /></div>
-                    <label for="gridColour" v-t="'game.ui.menu.menu.grid_color_set'"></label>
-                    <color-picker id="gridColour" :color.sync="gridColour" />
-                    <label for="fowColour" v-t="'game.ui.menu.menu.fow_color_set'"></label>
-                    <color-picker id="fowColour" :color.sync="fowColour" />
-                    <label for="rulerColour" v-t="'game.ui.menu.menu.ruler_color_set'"></label>
-                    <color-picker id="rulerColour" :color.sync="rulerColour" />
-                    <label for="invertAlt" v-t="'game.ui.menu.menu.invert_alt_set'"></label>
-                    <div><input id="invertAlt" type="checkbox" v-model="invertAlt" /></div>
-                    <label for="languageSelect" v-t="'locale.select'"></label>
-                    <div><LanguageSelect id="languageSelect" /></div>
-                </div>
-            </div>
+            <button class="menu-accordion" @click="openUserSettings" v-t="'game.ui.menu.menu.client_options'"></button>
         </div>
         <router-link
             to="/dashboard"
---
+++ client/src/game/ui/settings/client/BehaviourSettings.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import { gameStore } from "../../../store";
+
+@Component
+export default class BehaviourSettings extends Vue {
+    get invertAlt(): boolean {
+        return gameStore.invertAlt;
+    }
+
+    set invertAlt(value: boolean) {
+        gameStore.setInvertAlt({ invertAlt: value, sync: true });
+    }
+}
+</script>
+
+<template>
+    <div class="panel restore-panel">
+        <div class="spanrow header">Grid</div>
+        <div class="row">
+            <label for="invertAlt" v-t="'game.ui.settings.client.BehaviourSettings.invert_alt_set'"></label>
+            <div><input id="invertAlt" type="checkbox" v-model="invertAlt" /></div>
+            <div v-if="1 < 2" :title="$t('game.ui.settings.common.reset_default')">
+                <font-awesome-icon icon="times-circle" />
+            </div>
+            <div v-else></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* Force higher specificity without !important abuse */
+.panel.restore-panel {
+    min-width: 20vw;
+    column-gap: 15px;
+    grid-template-columns: [setting] 1fr [value] auto [restore] 30px [end];
+}
+
+.overwritten,
+.restore-panel .row.overwritten * {
+    color: #7c253e;
+    font-weight: bold;
+}
+
+.panel input[type="number"] {
+    width: 50px;
+}
+</style>

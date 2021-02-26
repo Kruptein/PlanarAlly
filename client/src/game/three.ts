72338195bd9cb28b7efddce6b8b66698214cd89c
Darragh Van Tichelen <info@darragh.dev>
2021-02-26 19:30:49 +0100

    Fix changelog

--- client/src/game/three.ts
+++
@@ -1,7 +0,0 @@
-import * as THREE from "three";
-window.THREE = THREE;
-require("three/examples/js/controls/OrbitControls.js");
-
-const OrbitControls = THREE.OrbitControls;
-export default THREE;
-export { OrbitControls };

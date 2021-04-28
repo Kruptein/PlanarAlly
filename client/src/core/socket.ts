646a7bbd39b906c96c630dec25044d50a7aa10d5
Darragh Van Tichelen <info@darragh.dev>
2021-04-28 16:35:57 +0200

    Remove some unused exports

--- client/src/core/socket.ts
+++ client/src/core/socket.ts
@@ -2,7 +2,7 @@ import { Manager } from "socket.io-client";
 
 import { BASE_PATH } from "../utils";
 
-export function createNewManager(): Manager {
+function createNewManager(): Manager {
     return new Manager(location.protocol + "//" + location.host, {
         autoConnect: false,
         path: BASE_PATH + "socket.io",
--- client/src/game/api/emits/initiative.ts
+++ client/src/game/api/emits/initiative.ts
@@ -3,7 +3,7 @@ import { wrapSocket } from "../helpers";
 
 export const sendInitiativeUpdate = wrapSocket<InitiativeData>("Initiative.Update");
 export const sendInitiativeRemove = wrapSocket<string>("Initiative.Remove");
-export const sendInitiativeSet = wrapSocket<string[]>("Initiative.Set");
+// export const sendInitiativeSet = wrapSocket<string[]>("Initiative.Set");
 export const sendInitiativeTurnUpdate = wrapSocket<string>("Initiative.Turn.Update");
 export const sendInitiativeRoundUpdate = wrapSocket<number>("Initiative.Round.Update");
 export const sendInitiativeNewEffect = wrapSocket<{ actor: string; effect: InitiativeEffect }>("Initiative.Effect.New");

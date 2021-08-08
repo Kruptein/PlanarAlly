dae7e7956ffa155cf63b7938eb52b25b4c966441
Darragh Van Tichelen <info@darragh.dev>
2021-08-08 10:12:50 +0200

    bug(Slider): unassigned ref messing with build

--- client/src/core/components/RotationSlider.vue
+++ client/src/core/components/RotationSlider.vue
@@ -66,7 +66,7 @@ function mouseMove(event: MouseEvent): void {
             @mousemove="mouseMove"
             @mouseleave="mouseUp"
         >
-            <div class="slider" ref="slider" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+            <div class="slider" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <div v-if="showNumberInput"><input type="number" v-model.number="degreeAngle" /></div>
     </div>
@@ -91,15 +91,15 @@ function mouseMove(event: MouseEvent): void {
         height: 20px;
         border: 2px solid gray;
         border-radius: 100%;
-    }
 
-    .slider {
-        position: relative;
-        height: 10px;
-        width: 10px;
-        background: gray;
-        border-radius: 100%;
-        cursor: pointer;
+        .slider {
+            position: relative;
+            height: 10px;
+            width: 10px;
+            background: gray;
+            border-radius: 100%;
+            cursor: pointer;
+        }
     }
 }
 </style>

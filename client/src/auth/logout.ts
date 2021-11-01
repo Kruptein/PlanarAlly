da6090de75d97e6ba9e0ea3915c33a2e033db2ec
Darragh Van Tichelen <info@darragh.dev>
2021-11-01 13:47:01 +0100

    Disable some new rules in specific cases

--- client/src/auth/logout.ts
+++ client/src/auth/logout.ts
@@ -4,6 +4,7 @@ import { postFetch } from "../core/utils";
 import { coreStore } from "../store/core";
 
 export const Logout = defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
     name: "Logout",
     async beforeRouteEnter(_to, _from, next) {
         await postFetch("/api/logout");

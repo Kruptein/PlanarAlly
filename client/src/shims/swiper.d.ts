0b3c3d822ecfef9ebcbd102a0ac72f17bdd05d49
Darragh Van Tichelen <info@darragh.dev>
2021-12-01 17:25:02 +0100

    Fix some swiper type issue with any for now

--- client/src/shims/swiper.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "swiper/vue" {
-    import component from "*.vue";
-    import _Vue from "vue";
-
-    export class Swiper extends component {}
-
-    export class SwiperSlide extends component {}
-}

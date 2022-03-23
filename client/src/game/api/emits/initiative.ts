de3760397f81023714150cb925f068107128d656
Darragh Van Tichelen <info@darragh.dev>
2022-03-23 09:58:34 +0100

    Fix more initiative id mixups

--- client/src/game/api/emits/initiative.ts
+++ client/src/game/api/emits/initiative.ts
@@ -1,20 +1,22 @@
+import type { GlobalId } from "../../id";
 import type { InitiativeData, InitiativeEffect, InitiativeSort } from "../../models/initiative";
 import { wrapSocket } from "../helpers";
 import { socket } from "../socket";
 
-export const sendInitiativeAdd = wrapSocket<InitiativeData>("Initiative.Add");
-export const sendInitiativeRemove = wrapSocket<string>("Initiative.Remove");
-export const sendInitiativeSetValue = wrapSocket<{ shape: string; value: number }>("Initiative.Value.Set");
+export const sendInitiativeAdd = wrapSocket<InitiativeData<GlobalId>>("Initiative.Add");
+export const sendInitiativeRemove = wrapSocket<GlobalId>("Initiative.Remove");
+export const sendInitiativeSetValue = wrapSocket<{ shape: GlobalId; value: number }>("Initiative.Value.Set");
 export const sendInitiativeTurnUpdate = wrapSocket<number>("Initiative.Turn.Update");
 export const sendInitiativeRoundUpdate = wrapSocket<number>("Initiative.Round.Update");
-export const sendInitiativeNewEffect = wrapSocket<{ actor: string; effect: InitiativeEffect }>("Initiative.Effect.New");
+export const sendInitiativeNewEffect =
+    wrapSocket<{ actor: GlobalId; effect: InitiativeEffect }>("Initiative.Effect.New");
 export const sendInitiativeRenameEffect =
-    wrapSocket<{ shape: string; index: number; name: string }>("Initiative.Effect.Rename");
+    wrapSocket<{ shape: GlobalId; index: number; name: string }>("Initiative.Effect.Rename");
 export const sendInitiativeTurnsEffect =
-    wrapSocket<{ shape: string; index: number; turns: string }>("Initiative.Effect.Turns");
-export const sendInitiativeRemoveEffect = wrapSocket<{ shape: string; index: number }>("Initiative.Effect.Remove");
+    wrapSocket<{ shape: GlobalId; index: number; turns: string }>("Initiative.Effect.Turns");
+export const sendInitiativeRemoveEffect = wrapSocket<{ shape: GlobalId; index: number }>("Initiative.Effect.Remove");
 export const sendInitiativeOptionUpdate =
-    wrapSocket<{ shape: string; option: string; value: boolean }>("Initiative.Option.Update");
+    wrapSocket<{ shape: GlobalId; option: string; value: boolean }>("Initiative.Option.Update");
 export const sendRequestInitiatives = (): void => {
     socket.emit("Initiative.Request");
 };
@@ -22,5 +24,5 @@ export const sendInitiativeClear = (): void => {
     socket.emit("Initiative.Clear");
 };
 export const sendInitiativeReorder =
-    wrapSocket<{ shape: string; oldIndex: number; newIndex: number }>("Initiative.Order.Change");
+    wrapSocket<{ shape: GlobalId; oldIndex: number; newIndex: number }>("Initiative.Order.Change");
 export const sendInitiativeSetSort = wrapSocket<InitiativeSort>("Initiative.Sort.Set");

5d7469e20804c620706a9bcd02a2a78b80525435
Darragh Van Tichelen <info@darragh.de>
2022-06-26 11:23:33 +0200

    Fix linting

--- client/src/game/ui/SelectionInfo.vue
+++ client/src/game/ui/SelectionInfo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from "@vue/reactivity";
+import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 
 import { SERVER_SYNC } from "../../core/models/types";

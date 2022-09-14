83fa1957d25ea093ba4633633d73c5eb5dce3532
Darragh Van Tichelen <info@darragh.dev>
2022-09-14 10:58:44 +0200

    Remove unused dashboard types

--- client/src/dashboard/types.ts
+++ client/src/dashboard/types.ts
@@ -4,34 +4,3 @@ export interface RoomInfo {
     logo?: string;
     is_locked: boolean;
 }
-
-export enum Navigation {
-    Play,
-    Run,
-    Create,
-    Import,
-    Settings,
-    AssetManage,
-    AssetCreate,
-    Logout,
-
-    Account,
-    Back,
-}
-
-type NavigationHeader = {
-    text: string;
-    type: "header";
-};
-
-type NavigationSeparator = {
-    type: "separator";
-};
-
-type NavigationAction = {
-    type: "action";
-    navigation: Navigation;
-    fn: (navigation: Navigation) => void;
-};
-
-export type NavigationEntry = NavigationHeader | NavigationSeparator | NavigationAction;

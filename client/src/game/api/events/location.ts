6ce428a6c47bd7c401689c4fd74ac52212fe5417
Darragh Van Tichelen <info@darragh.dev>
2022-10-11 14:21:17 +0200

    Fix location settings overwrite mode

--- client/src/game/api/events/location.ts
+++ client/src/game/api/events/location.ts
@@ -20,14 +20,15 @@ socket.on("Location.Set", (data: ServerLocation) => {
 
 socket.on("Locations.Settings.Set", (data: ServerLocationInfo) => {
     locationSettingsSystem.setActiveLocation(data.active);
-    setLocationOptions(undefined, data.default);
-    for (const key in data.locations) setLocationOptions(Number.parseInt(key), data.locations[key]);
+    setLocationOptions(undefined, data.default, true);
+    for (const key in data.locations) setLocationOptions(Number.parseInt(key), data.locations[key], true);
 });
 
 // Varia
 
+// This is used to set 1 or more settings, not ALL settings!
 socket.on("Location.Options.Set", (data: { options: Partial<ServerLocationOptions>; location: number | null }) => {
-    setLocationOptions(data.location ?? undefined, data.options);
+    setLocationOptions(data.location ?? undefined, data.options, false);
 });
 
 socket.on("Locations.Order.Set", (locations: Location[]) => {
@@ -43,40 +44,55 @@ socket.on("Location.Rename", (data: { location: number; name: string }) => {
     locationStore.renameLocation(data.location, data.name, false);
 });
 
-function setLocationOptions(id: number | undefined, options: Partial<ServerLocationOptions>): void {
+function setLocationOptions(
+    id: number | undefined,
+    options: Partial<ServerLocationOptions>,
+    overwrite_all: boolean,
+): void {
     // GRID
-    locationSettingsSystem.setUseGrid(options.use_grid, id, false);
-    locationSettingsSystem.setGridType(options.grid_type, id, false);
-    locationSettingsSystem.setUnitSize(options.unit_size, id, false);
-    locationSettingsSystem.setUnitSizeUnit(options.unit_size_unit, id, false);
+    if (overwrite_all || options.use_grid !== undefined) locationSettingsSystem.setUseGrid(options.use_grid, id, false);
+    if (overwrite_all || options.grid_type !== undefined)
+        locationSettingsSystem.setGridType(options.grid_type, id, false);
+    if (overwrite_all || options.unit_size !== undefined)
+        locationSettingsSystem.setUnitSize(options.unit_size, id, false);
+    if (overwrite_all || options.unit_size_unit !== undefined)
+        locationSettingsSystem.setUnitSizeUnit(options.unit_size_unit, id, false);
 
     // VISION
 
-    locationSettingsSystem.setFullFow(options.full_fow, id, false);
-    locationSettingsSystem.setFowLos(options.fow_los, id, false);
-    locationSettingsSystem.setFowOpacity(options.fow_opacity, id, false);
+    if (overwrite_all || options.full_fow !== undefined) locationSettingsSystem.setFullFow(options.full_fow, id, false);
+    if (overwrite_all || options.fow_los !== undefined) locationSettingsSystem.setFowLos(options.fow_los, id, false);
+    if (overwrite_all || options.fow_opacity !== undefined)
+        locationSettingsSystem.setFowOpacity(options.fow_opacity, id, false);
 
-    const visionMode = options.vision_mode !== undefined ? visibilityModeFromString(options.vision_mode) : undefined;
-    if (visionMode !== undefined) visionState.setVisionMode(visionMode, false);
+    if (overwrite_all || options.vision_mode !== undefined) {
+        const visionMode =
+            options.vision_mode !== undefined ? visibilityModeFromString(options.vision_mode) : undefined;
+        if (visionMode !== undefined) visionState.setVisionMode(visionMode, false);
+    }
 
-    locationSettingsSystem.setVisionMinRange(options.vision_min_range, id, false);
-    locationSettingsSystem.setVisionMaxRange(options.vision_max_range, id, false);
+    if (overwrite_all || options.vision_min_range !== undefined)
+        locationSettingsSystem.setVisionMinRange(options.vision_min_range, id, false);
+    if (overwrite_all || options.vision_max_range !== undefined)
+        locationSettingsSystem.setVisionMaxRange(options.vision_max_range, id, false);
 
     // FLOOR
 
-    locationSettingsSystem.setAirMapBackground(options.air_map_background, id, false);
-
-    locationSettingsSystem.setGroundMapBackground(options.ground_map_background, id, false);
-
-    locationSettingsSystem.setUndergroundMapBackground(options.underground_map_background, id, false);
+    if (overwrite_all || options.air_map_background !== undefined)
+        locationSettingsSystem.setAirMapBackground(options.air_map_background, id, false);
+    if (overwrite_all || options.ground_map_background !== undefined)
+        locationSettingsSystem.setGroundMapBackground(options.ground_map_background, id, false);
+    if (overwrite_all || options.underground_map_background !== undefined)
+        locationSettingsSystem.setUndergroundMapBackground(options.underground_map_background, id, false);
 
     // VARIA
 
-    locationSettingsSystem.setMovePlayerOnTokenChange(options.move_player_on_token_change, id, false);
+    if (overwrite_all || options.move_player_on_token_change !== undefined)
+        locationSettingsSystem.setMovePlayerOnTokenChange(options.move_player_on_token_change, id, false);
 
     // SPAWN LOCATIONS
 
-    if (id !== undefined) {
+    if (id !== undefined && (overwrite_all || options.spawn_locations !== undefined)) {
         const spawnLocations: GlobalId[] = JSON.parse(options.spawn_locations ?? "[]");
         locationSettingsSystem.setSpawnLocations(
             spawnLocations.map((s) => reserveLocalId(s)!),

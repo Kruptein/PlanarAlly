5680140e45c7b1c526a52610ae44b72d701db661
Darragh Van Tichelen <info@darragh.dev>
2022-11-06 08:42:08 +0100

    Add some extra debug code (showLayerPoints)

--- client/src/game/vision/state.ts
+++ client/src/game/vision/state.ts
@@ -140,8 +140,8 @@ class VisionState extends Store<State> {
         const shapes = this.getBlockers(target, floor);
 
         for (const sh of shapes) {
-            const shape = getShape(sh)!;
-            if (shape.floor.id !== floor) continue;
+            const shape = getShape(sh);
+            if (shape === undefined || shape.floor.id !== floor) continue;
 
             this.triangulateShape(target, shape);
         }

266fc930ff29ca5614add90f80e69992221b6241
Darragh Van Tichelen <info@darragh.dev>
2023-01-19 16:58:03 +0100

    lg done

--- server/src/api/socket/lg.py
+++ server/src/api/socket/lg.py
@@ -1,14 +1,8 @@
-from typing_extensions import TypedDict
-
 from ... import auth
 from ...app import app, sio
 from ...state.game import game_state
+from ..helpers import _send_game
 from .constants import GAME_NS
-
-
-class LgUuidLink(TypedDict):
-    typeId: int
-    uuid: str
 
 
 @sio.on("Lg.Token.Connect", namespace=GAME_NS)
@@ -17,7 +11,7 @@ async def token_connect(sid: str, data: int):
     pr = game_state.get(sid)
 
     for psid in game_state.get_sids(room=pr.room, skip_sid=sid):
-        await sio.emit("Lg.Token.Connect", data, room=psid, namespace=GAME_NS)
+        await _send_game("Lg.Token.Connect", data, room=psid)
 
 
 @sio.on("Lg.Grid.Ids.Show", namespace=GAME_NS)
@@ -25,9 +19,7 @@ async def token_connect(sid: str, data: int):
 async def show_grid_ids(sid: str):
     pr = game_state.get(sid)
 
-    await sio.emit(
-        "Lg.Grid.Ids.Show", room=pr.room.get_path(), skip_sid=sid, namespace=GAME_NS
-    )
+    await _send_game("Lg.Grid.Ids.Show", None, room=pr.room.get_path(), skip_sid=sid)
 
 
 @sio.on("Lg.Grid.Ids.Hide", namespace=GAME_NS)
@@ -35,6 +27,5 @@ async def show_grid_ids(sid: str):
 async def hide_grid_ids(sid: str):
     pr = game_state.get(sid)
 
-    await sio.emit(
-        "Lg.Grid.Ids.Hide", room=pr.room.get_path(), skip_sid=sid, namespace=GAME_NS
-    )
+    await _send_game("Lg.Grid.Ids.Hide", None, room=pr.room.get_path(), skip_sid=sid)
+    await _send_game("Lg.Grid.Ids.Hide", None, room=pr.room.get_path(), skip_sid=sid)

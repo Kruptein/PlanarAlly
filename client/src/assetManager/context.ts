09eff6419ccdb549902c1764952eddc5c8092f2e
Darragh Van Tichelen <info@darragh.dev>
2023-02-14 09:06:21 +0100

    bug(AssetManager): Fix context menu appearing in wrong location when scrolled

--- client/src/assetManager/context.ts
+++ client/src/assetManager/context.ts
@@ -6,6 +6,6 @@ export const assetContextTop = ref(0);
 
 export function openAssetContextMenu(event: MouseEvent): void {
     showAssetContextMenu.value = true;
-    assetContextLeft.value = event.pageX;
-    assetContextTop.value = event.pageY;
+    assetContextLeft.value = event.clientX;
+    assetContextTop.value = event.clientY;
 }

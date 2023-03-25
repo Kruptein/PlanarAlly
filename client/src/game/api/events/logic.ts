489e35cbee187f39a46bb947a56946c720c1c47a
Darragh Van Tichelen <info@darragh.dev>
2023-03-25 12:30:09 +0100

    Another fix for player initiated tps

--- client/src/game/api/events/logic.ts
+++ client/src/game/api/events/logic.ts
@@ -1,27 +1,19 @@
 import { POSITION, useToast } from "vue-toastification";
 
 import type { LogicRequestInfo } from "../../../apiTypes";
+import { defined, filter, guard, map } from "../../../core/iter";
 import { getLocalId } from "../../id";
 import { doorSystem } from "../../systems/logic/door";
 import { Access } from "../../systems/logic/models";
+import { teleportZoneSystem } from "../../systems/logic/tp";
+import { teleport } from "../../systems/logic/tp/core";
 // eslint-disable-next-line import/default
 import LogicRequestHandlerToast from "../../ui/toasts/LogicRequestHandlerToast.vue";
 import { socket } from "../socket";
 
 const toast = useToast();
 
-socket.on("Logic.Request", (data: LogicRequestInfo) => {
-    if (data.request.logic === "door") {
-        const doorId = getLocalId(data.request.door);
-        if (doorId === undefined) return;
-        const canUse = doorSystem.canUse(doorId, data.requester);
-        if (canUse === Access.Enabled) {
-            doorSystem.toggleDoor(doorId);
-            return;
-        } else if (canUse === Access.Disabled) {
-            return;
-        }
-    }
+function showRequestHandler(data: LogicRequestInfo): void {
     toast.info(
         { component: LogicRequestHandlerToast, props: { data } },
         {
@@ -30,6 +22,39 @@ socket.on("Logic.Request", (data: LogicRequestInfo) => {
             closeOnClick: false,
         },
     );
+}
+
+socket.on("Logic.Request", async (data: LogicRequestInfo) => {
+    if (data.request.logic === "door") {
+        const doorId = getLocalId(data.request.door);
+        if (doorId === undefined) return;
+
+        const canUse = doorSystem.canUse(doorId, data.requester);
+
+        if (canUse === Access.Enabled) {
+            doorSystem.toggleDoor(doorId);
+        } else if (canUse === Access.Request) {
+            showRequestHandler(data);
+        }
+    } else {
+        const tpId = getLocalId(data.request.fromZone);
+        if (tpId === undefined) return;
+
+        const canUse = teleportZoneSystem.canUse(tpId, data.requester);
+
+        if (canUse === Access.Enabled) {
+            const targets = guard(
+                filter(
+                    map(data.request.transfers, (m) => getLocalId(m)),
+                    (i) => i !== undefined,
+                ),
+                defined,
+            );
+            await teleport(tpId, data.request.toZone, [...targets]);
+        } else if (canUse === Access.Request) {
+            showRequestHandler(data);
+        }
+    }
 });
 
 socket.on("Logic.Request.Declined", () => {

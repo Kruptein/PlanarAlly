ef38a05885b4227322915eced02bb5828ab744d8
Darragh Van Tichelen <info@darragh.dev>
2023-04-04 20:11:12 +0200

    Fix creation of new polygons breaking

--- client/src/game/shapes/create.ts
+++ client/src/game/shapes/create.ts
@@ -42,70 +42,79 @@ export function createShapeFromDict(shape: ApiShape): IShape | undefined {
 
     // Shape Type specifics
 
-    const refPoint = toGP(shape.x, shape.y);
-    if (shape.type_ === "rect") {
-        const rect = shape as ApiRectShape;
-        sh = new Rect(refPoint, rect.width, rect.height, {
-            uuid,
-        });
-    } else if (shape.type_ === "circle") {
-        const circ = shape as ApiCircleShape;
-        sh = new Circle(refPoint, circ.radius, {
-            uuid,
-        });
-    } else if (shape.type_ === "circulartoken") {
-        const token = shape as ApiCircularTokenShape;
-        sh = new CircularToken(refPoint, token.radius, token.text, token.font, {
-            uuid,
-        });
-    } else if (shape.type_ === "line") {
-        const line = shape as ApiLineShape;
-        sh = new Line(refPoint, toGP(line.x2, line.y2), {
-            lineWidth: line.line_width,
-            uuid,
-        });
-    } else if (shape.type_ === "polygon") {
-        const polygon = shape as ApiPolygonShape;
-        const vertices = JSON.parse(polygon.vertices) as [number, number][];
-        sh = new Polygon(
-            refPoint,
-            vertices.map((v) => toGP(v)),
-            {
-                lineWidth: [polygon.line_width],
-                openPolygon: polygon.open_polygon,
+    try {
+        const refPoint = toGP(shape.x, shape.y);
+        if (shape.type_ === "rect") {
+            const rect = shape as ApiRectShape;
+            sh = new Rect(refPoint, rect.width, rect.height, {
                 uuid,
-            },
-        );
-    } else if (shape.type_ === "text") {
-        const text = shape as ApiTextShape;
-        sh = new Text(refPoint, text.text, text.font_size, {
-            uuid,
-        });
-    } else if (shape.type_ === "assetrect") {
-        const asset = shape as ApiAssetRectShape;
-        const img = new Image(asset.width, asset.height);
-        if (asset.src.startsWith("http")) img.src = baseAdjust(new URL(asset.src).pathname);
-        else img.src = baseAdjust(asset.src);
-        sh = new Asset(img, refPoint, asset.width, asset.height, { uuid, loaded: false });
-        img.onload = () => {
-            (sh as Asset).setLoaded();
-        };
-    } else if (shape.type_ === "togglecomposite") {
-        const toggleComposite = shape as ApiToggleCompositeShape;
-
-        if (toggleComposite.active_variant === null) throw new Error("ToggleComposite with no active variant found");
-
-        sh = new ToggleComposite(
-            refPoint,
-            getLocalId(toggleComposite.active_variant)!,
-            toggleComposite.variants.map((v) => ({ id: reserveLocalId(v.uuid), name: v.name })),
-            {
+            });
+        } else if (shape.type_ === "circle") {
+            const circ = shape as ApiCircleShape;
+            sh = new Circle(refPoint, circ.radius, {
                 uuid,
-            },
-        );
-    } else {
+            });
+        } else if (shape.type_ === "circulartoken") {
+            const token = shape as ApiCircularTokenShape;
+            sh = new CircularToken(refPoint, token.radius, token.text, token.font, {
+                uuid,
+            });
+        } else if (shape.type_ === "line") {
+            const line = shape as ApiLineShape;
+            sh = new Line(refPoint, toGP(line.x2, line.y2), {
+                lineWidth: line.line_width,
+                uuid,
+            });
+        } else if (shape.type_ === "polygon") {
+            const polygon = shape as ApiPolygonShape;
+            const vertices = JSON.parse(polygon.vertices) as [number, number][];
+            sh = new Polygon(
+                refPoint,
+                vertices.map((v) => toGP(v)),
+                {
+                    lineWidth: [polygon.line_width],
+                    openPolygon: polygon.open_polygon,
+                    uuid,
+                },
+            );
+        } else if (shape.type_ === "text") {
+            const text = shape as ApiTextShape;
+            sh = new Text(refPoint, text.text, text.font_size, {
+                uuid,
+            });
+        } else if (shape.type_ === "assetrect") {
+            const asset = shape as ApiAssetRectShape;
+            const img = new Image(asset.width, asset.height);
+            if (asset.src.startsWith("http")) img.src = baseAdjust(new URL(asset.src).pathname);
+            else img.src = baseAdjust(asset.src);
+            sh = new Asset(img, refPoint, asset.width, asset.height, { uuid, loaded: false });
+            img.onload = () => {
+                (sh as Asset).setLoaded();
+            };
+        } else if (shape.type_ === "togglecomposite") {
+            const toggleComposite = shape as ApiToggleCompositeShape;
+
+            if (toggleComposite.active_variant === null)
+                throw new Error("ToggleComposite with no active variant found");
+
+            sh = new ToggleComposite(
+                refPoint,
+                getLocalId(toggleComposite.active_variant)!,
+                toggleComposite.variants.map((v) => ({ id: reserveLocalId(v.uuid), name: v.name })),
+                {
+                    uuid,
+                },
+            );
+        } else {
+            console.error(`Failed to create Shape with unknown type ${shape.type_}`);
+            return undefined;
+        }
+    } catch (exception) {
+        console.error(`Failed to create Shape of type ${shape.type_}`);
+        console.error(exception);
         return undefined;
     }
+
     sh.fromDict(shape);
     return sh;
 }

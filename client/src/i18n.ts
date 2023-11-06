be43f513c826f32d9f638f1fde60a1953df6d0ce
Xhq <xuhaoqing2013@hotmail.com>
2023-11-07 02:42:17 +0800

    change default language

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -4,7 +4,7 @@ import type { I18nOptions } from "vue-i18n";
 
 export const i18n = createI18n({
     legacy: false,
-    locale: localStorage.getItem("locale") ?? (import.meta.env.VITE_APP_I18N_LOCALE as string | undefined) ?? "en",
-    fallbackLocale: (import.meta.env.VITE_APP_I18N_FALLBACK_LOCALE as string | undefined) ?? "en",
+    locale: localStorage.getItem("locale") ?? (import.meta.env.VITE_APP_I18N_LOCALE as string | undefined) ?? "zh",
+    fallbackLocale: (import.meta.env.VITE_APP_I18N_FALLBACK_LOCALE as string | undefined) ?? "zh",
     messages: messages as I18nOptions["messages"],
 });

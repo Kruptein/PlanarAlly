3f3b4855ee21cfad77b01912cea753e8daff98b4
Darragh Van Tichelen <info@darragh.dev>
2024-01-28 11:28:19 +0100

    WIP

--- client/src/game/tools/variants/ruler.ts
+++ client/src/game/tools/variants/ruler.ts
@@ -1,7 +1,7 @@
 import { ref } from "vue";
 
 import { l2g } from "../../../core/conversions";
-import { cloneP, toGP } from "../../../core/geometry";
+import { Ray, cloneP, equalsP, toGP } from "../../../core/geometry";
 import type { GlobalPoint, LocalPoint } from "../../../core/geometry";
 import { DEFAULT_GRID_SIZE, snapPointToGrid } from "../../../core/grid";
 import { InvalidationMode, NO_SYNC, SyncMode } from "../../../core/models/types";
@@ -10,7 +10,9 @@ import { sendShapePositionUpdate } from "../../api/emits/shape/core";
 import { LayerName } from "../../models/floor";
 import { ToolName } from "../../models/tools";
 import type { ITool, ToolFeatures, ToolPermission } from "../../models/tools";
+import { Circle } from "../../shapes/variants/circle";
 import { Line } from "../../shapes/variants/line";
+import { Rect } from "../../shapes/variants/rect";
 import { Text } from "../../shapes/variants/text";
 import { accessSystem } from "../../systems/access";
 import { floorSystem } from "../../systems/floors";
@@ -23,6 +25,11 @@ import { Tool } from "../tool";
 
 export enum RulerFeatures {
     All,
+}
+
+function getCellCenter(point: GlobalPoint): GlobalPoint {
+    const gs = DEFAULT_GRID_SIZE;
+    return toGP(Math.floor(point.x / gs) * gs + gs / 2, Math.floor(point.y / gs) * gs + gs / 2);
 }
 
 class RulerTool extends Tool implements ITool {
@@ -40,6 +47,9 @@ class RulerTool extends Tool implements ITool {
 
     private currentLength = 0;
     private previousLength = 0;
+
+    private highlightedCells: Rect[] = [];
+    private highlightedCellss: Circle[] = [];
 
     get permittedTools(): ToolPermission[] {
         return [{ name: ToolName.Select, features: { enabled: [SelectFeatures.Context] } }];
@@ -126,6 +136,15 @@ class RulerTool extends Tool implements ITool {
             NO_SYNC,
         );
         layer.addShape(this.text, this.syncMode, InvalidationMode.NORMAL);
+
+        const cellCenter = getCellCenter(this.startPoint);
+        const rect = new Rect(cellCenter, DEFAULT_GRID_SIZE, DEFAULT_GRID_SIZE, undefined, {
+            fillColour: "lightblue",
+        });
+        rect.center = cellCenter;
+        this.highlightedCells.push(rect);
+        layer.addShape(rect, SyncMode.NO_SYNC, InvalidationMode.NORMAL);
+
         return Promise.resolve();
     }
 
@@ -149,6 +168,40 @@ class RulerTool extends Tool implements ITool {
             });
         }
 
+        for (const shape of this.highlightedCells) {
+            layer.removeShape(shape, { sync: SyncMode.NO_SYNC, recalculate: false, dropShapeId: true });
+        }
+        this.highlightedCells = [];
+        for (const shape of this.highlightedCellss) {
+            layer.removeShape(shape, { sync: SyncMode.NO_SYNC, recalculate: false, dropShapeId: true });
+        }
+        this.highlightedCellss = [];
+        const ray = Ray.fromPoints(getCellCenter(this.startPoint), getCellCenter(endPoint));
+        // const iterations = Math.round(ray.getDistance(0, ray.tMax) / DEFAULT_GRID_SIZE);
+        const iterations = Math.round(
+            Math.max(
+                Math.abs(endPoint.x - this.startPoint.x) / DEFAULT_GRID_SIZE,
+                Math.abs(endPoint.y - this.startPoint.y) / DEFAULT_GRID_SIZE,
+            ),
+        );
+        const step = ray.tMax / iterations;
+        for (let i = 0; i <= iterations; i++) {
+            const cellCenter = getCellCenter(ray.get(step * i));
+            const lastCenter = this.highlightedCells.at(-1)?.center;
+            if (lastCenter !== undefined && equalsP(cellCenter, lastCenter)) continue;
+            const rect = new Rect(cellCenter, DEFAULT_GRID_SIZE, DEFAULT_GRID_SIZE, undefined, {
+                fillColour: "rgba(173, 216, 230, 0.25)",
+            });
+            rect.center = cellCenter;
+            this.highlightedCells.push(rect);
+            layer.addShape(rect, SyncMode.NO_SYNC, InvalidationMode.NORMAL);
+        }
+        for (let i = 0; i <= iterations; i++) {
+            const blob = new Circle(ray.get(step * i), 5, undefined, { fillColour: "red" });
+            this.highlightedCellss.push(blob);
+            layer.addShape(blob, SyncMode.NO_SYNC, InvalidationMode.NORMAL);
+        }
+
         const ruler = this.rulers.at(-1)!;
         ruler.endPoint = endPoint;
         sendShapePositionUpdate([ruler], true);
@@ -159,14 +212,22 @@ class RulerTool extends Tool implements ITool {
         const diffsign = Math.sign(end.x - start.x) * Math.sign(end.y - start.y);
         const xdiff = Math.abs(end.x - start.x);
         const ydiff = Math.abs(end.y - start.y);
-        let distance =
-            (Math.sqrt(xdiff ** 2 + ydiff ** 2) * locationSettingsState.raw.unitSize.value) / DEFAULT_GRID_SIZE;
+        // let distance =
+        // (Math.sqrt(xdiff ** 2 + ydiff ** 2) * locationSettingsState.raw.unitSize.value) / DEFAULT_GRID_SIZE;
+        let distance = this.highlightedCells.length;
         this.currentLength = distance;
         distance += this.previousLength;
 
         // round to 1 decimal
+        // const label =
+        //     i18n.global.n(Math.round(10 * distance) / 10) + " " + locationSettingsState.raw.unitSizeUnit.value;
         const label =
-            i18n.global.n(Math.round(10 * distance) / 10) + " " + locationSettingsState.raw.unitSizeUnit.value;
+            i18n.global.n(distance) +
+            " (" +
+            distance * locationSettingsState.raw.unitSize.value +
+            " " +
+            locationSettingsState.raw.unitSizeUnit.value +
+            ")";
         const angle = Math.atan2(diffsign * ydiff, xdiff);
         const xmid = Math.min(start.x, end.x) + xdiff / 2;
         const ymid = Math.min(start.y, end.y) + ydiff / 2;
@@ -174,6 +235,7 @@ class RulerTool extends Tool implements ITool {
         this.text.setText(label, SyncMode.TEMP_SYNC);
         this.text.angle = angle;
         sendShapePositionUpdate([this.text], true);
+
         layer.invalidate(true);
         return Promise.resolve();
     }

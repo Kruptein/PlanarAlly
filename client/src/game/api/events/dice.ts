8c4a8803a061f5c53566998be75e4e273dbdf1d0
Darragh Van Tichelen <info@darragh.dev>
2024-07-28 20:15:41 +0200

    Show dice roll in roll notifications

--- client/src/game/api/events/dice.ts
+++ client/src/game/api/events/dice.ts
@@ -7,7 +7,7 @@ import { socket } from "../socket";
 const toast = useToast();
 
 socket.on("Dice.Roll.Result", (data: DiceRollResult) => {
-    toast.info(`${data.player} threw a ${data.result}`, {
+    toast.info(`${data.player} rolled ${data.roll} and threw a ${data.result}`, {
         position: POSITION.TOP_RIGHT,
     });
     diceTool.state.history.push(data);

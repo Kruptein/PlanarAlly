18615b3c8128f2aff2b65ea114357fbcc33679cf
Darragh Van Tichelen <info@darragh.dev>
2024-07-28 20:16:33 +0200

    Remove more gameboard code

--- client/src/game/tools/variants/dice.ts
+++ client/src/game/tools/variants/dice.ts
@@ -7,15 +7,12 @@ import { reactive, watch } from "vue";
 
 import { randomInterval } from "../../../core/utils";
 import { i18n } from "../../../i18n";
-import { coreStore } from "../../../store/core";
 import { diceStore } from "../../dice/state";
 import { ToolName } from "../../models/tools";
 import type { ITool, ToolPermission } from "../../models/tools";
 import { playerSettingsState } from "../../systems/settings/players/state";
 import { SelectFeatures } from "../models/select";
 import { Tool } from "../tool";
-
-const hasGameboard = coreStore.state.boardId !== undefined;
 
 class DiceTool extends Tool implements ITool {
     readonly toolName = ToolName.Dice;
@@ -59,7 +56,6 @@ class DiceTool extends Tool implements ITool {
         inp: string,
         options?: { color?: string; startPosition?: [number, number]; throwKey: string },
     ): Promise<string> {
-        if (!hasGameboard) diceStore.setIsPending(true);
         if (options?.throwKey !== undefined && options.throwKey in this.state.timeouts) {
             clearTimeout(this.state.timeouts[options.throwKey]);
             delete this.state.timeouts[options.throwKey];
@@ -77,28 +73,7 @@ class DiceTool extends Tool implements ITool {
         const targetColor = options?.color ?? playerSettingsState.raw.rulerColour.value;
         const color = tinycolor(targetColor).toHexString();
 
-        let position = new Vector3(signX * (side ? 0.9 * w : xDir * w), 4.5, signY * (side ? yDir * h : 0.9 * h));
-        /*
-        babylon:
-        (maxX, minY)   (minX, minY)
-                    0,0
-        (maxX, maxY)   (minX, maxY)
-
-        gameboard:
-        (0, 0)   (1, 0)
-        (0, 1)   (1, 1)
-        */
-        if (hasGameboard && options?.startPosition !== undefined) {
-            if (options.startPosition[0] === undefined || options.startPosition[1] === undefined) {
-                console.error("DICE ROLL START_POSITION WAS PASSED BUT INVALID");
-            } else {
-                position = new Vector3(
-                    2 * w * (0.5 - options.startPosition[0]),
-                    4.5,
-                    2 * h * (options.startPosition[1] - 0.5),
-                );
-            }
-        }
+        const position = new Vector3(signX * (side ? 0.9 * w : xDir * w), 4.5, signY * (side ? yDir * h : 0.9 * h));
 
         // Aim from side to center
         const linear = Vector3.Zero()
@@ -110,9 +85,7 @@ class DiceTool extends Tool implements ITool {
         const dieOptions: Omit<DieOptions, "die"> = {
             position,
             linear,
-            angular: hasGameboard
-                ? new Vector3(randomInterval(-3, 3), randomInterval(-3, 3), randomInterval(-3, 3))
-                : new Vector3(linear.x / 2, 0, 0),
+            angular: new Vector3(linear.x / 2, 0, 0),
             color,
         };
 
@@ -122,12 +95,8 @@ class DiceTool extends Tool implements ITool {
             key: options?.throwKey,
             resetAllDice: false,
         });
-        window.GameboardAnalytics?.sendEvent("DICE_ROLL_RESULTS", JSON.stringify(results));
         diceStore.setResults(results.key, results.data, options?.startPosition);
-        if (!hasGameboard) {
-            diceStore.setIsPending(false);
-            diceStore.setShowDiceResults(results.key);
-        }
+
         const timeoutId = window.setTimeout(() => {
             diceStore
                 .getDiceThrower()

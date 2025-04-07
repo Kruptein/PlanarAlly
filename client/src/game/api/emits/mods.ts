0084b2999e54f9a43441e71571a287ee13786fac
Darragh Van Tichelen <info@darragh.dev>
2025-04-07 18:36:07 +0200

    uncomment currently unused sendLinkModToClient emit

--- client/src/game/api/emits/mods.ts
+++ client/src/game/api/emits/mods.ts
@@ -4,5 +4,5 @@ import { wrapSocket, wrapSocketWithAck } from "../helpers";
 export const sendGetRoomMods = wrapSocketWithAck<ApiModMeta[]>("Mods.Room.Get");
 
 export const sendLinkModToRoom = wrapSocket<ApiModLink>("Mods.Room.Link");
-export const sendLinkModToClient = wrapSocket<ApiModLink>("Mods.Room.LinkUser");
+// export const sendLinkModToClient = wrapSocket<ApiModLink>("Mods.Room.LinkUser");
 export const sendRemoveModFromRoom = wrapSocket<ApiModLink>("Mods.Room.Remove");

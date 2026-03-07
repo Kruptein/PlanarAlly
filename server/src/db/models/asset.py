402ccecb9a11a6df675c7d341391108da75e6887
Darragh Van Tichelen <info@darragh.dev>
2026-03-07 14:48:10 +0100

    Small asset improvements

--- server/src/db/models/asset.py
+++ server/src/db/models/asset.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING, Literal, cast
 
-from peewee import TextField
+from peewee import IntegerField, TextField
 
 
 from ...logs import logger
@@ -12,7 +12,7 @@ from .user import User
 
 if TYPE_CHECKING:
     from .asset_entry import AssetEntry
-    from .shape import Shape
+    from .asset_rect import AssetRect
     from .shape_template import ShapeTemplate
 
 
@@ -21,12 +21,13 @@ class Asset(BaseDbModel):
 
     # !! When links are added update the cleanup function !!
     entries: SelectSequence["AssetEntry"]
-    shapes: SelectSequence["Shape"]
+    asset_rects: SelectSequence["AssetRect"]
     templates: SelectSequence["ShapeTemplate"]
 
     file_hash = cast(str, TextField())
     kind = cast(Literal["regular", "ddraft"], TextField())
     extension = cast(str | None, TextField(null=True))
+    file_size = cast(int | None, IntegerField(null=True))
 
     def __repr__(self):
         return f"<Asset {self.file_hash}>"
@@ -34,9 +35,11 @@ class Asset(BaseDbModel):
     def cleanup_check(self):
         full_hash_path = get_asset_hash_subpath(self.file_hash)
         if (ASSETS_DIR / full_hash_path).exists():
-            if self.entries.count() == 0 and self.shapes.count() == 0 and self.templates.count() == 0:
+            if self.entries.count() == 0 and self.asset_rects.count() == 0 and self.templates.count() == 0:
                 logger.info(f"No data maps to file {self.file_hash}, removing from server")
                 (ASSETS_DIR / full_hash_path).unlink()
+                for suffix in [".thumb.webp", ".thumb.jpeg"]:
+                    (ASSETS_DIR / f"{full_hash_path}{suffix}").unlink(missing_ok=True)
 
     def generate_thumbnails(self) -> None:
         generate_thumbnail_for_asset(self.file_hash)
